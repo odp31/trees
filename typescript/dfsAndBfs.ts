4c9f836e052006e0cce621aec8516952fefcb2cd
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-12-19 12:51:48 -0500

    Update dfsAndBfs.ts

--- typescript/dfsAndBfs.ts
+++ typescript/dfsAndBfs.ts
@@ -26,3 +26,24 @@ function bfs<T>(graph: Map<T, T[]>, start: T): void {
     }
   }
 }
+
+
+// example usage
+const graph = new Map<string, string[]>();
+graph.set('A', ['B', 'C']);
+graph.set('B', ['D', 'E']);
+graph.set('C', ['F']);
+graph.set('D', ['G']);
+graph.set('E', ['H']);
+graph.set('F', ['I']);
+graph.set('G', ['J']);
+graph.set('H', []);
+graph.set('I', []);
+graph.set('J', []);
+
+console.log("dfs traversal: ");
+dfs(graph, 'A');
+
+console.log("\nBFS traversal:");
+bfs(graph, 'A');
+
